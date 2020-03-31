0306bcf812b081bb567425d79ee98916e508fb73
starlikerain <Zxcvbnm12123>
2020-03-31 09:58:28 +0800

    consumer

--- src/com/company/lambda/main.java
+++ src/com/company/lambda/main.java
@@ -1,28 +1,35 @@
 package com.company.lambda;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * 可以用 lambda 的特性，做性能优化
  * lambda 有延迟加载的功能，必须存在函数式接口
  */
 
 public class main {
-    public static void showMessage(int level, MessageBuilder mb) {
-        if (level == 1) {
-            System.out.println(mb.buildMessage());
+    public static void consumer(String[] names, Consumer<String> consumer1, Consumer<String> consumer2) {
+        for (String name : names) {
+            consumer1.andThen(consumer2).accept(name);
         }
-
     }
 
-
     public static void main(String[] args) {
-        String msg1 = "我";
-        String msg2 = "你";
-        String msg3 = "ta";
+        String[] names = {"迪丽热巴,女","段,女","哈撒给,男"};
 
-        showMessage(2, () -> {
-            System.out.println("不满足条件不执行");
-            return msg1 + msg2 + msg3;
-        });
-
+        consumer(names,
+                (name) -> {
+                    String[] nameAndSex = name.split(",");
+                    System.out.print("名称是:"+ nameAndSex[0]);
+                },
+                (name) -> {
+                    String[] nameAndSex = name.split(",");
+                    System.out.println("性别是:"+ nameAndSex[1]);
+                }
+                );
     }
 }

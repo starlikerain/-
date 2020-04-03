7e01389aa6ca9931f756e3b4fa1e481e32d03ec5
starlikerain <Zxcvbnm12123>
2020-04-03 17:30:41 +0800

    反射暴力获取

--- src/test/OtherClass/Person.java
+++ src/test/OtherClass/Person.java
@@ -26,4 +26,13 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", a='" + a + '\'' +
+                '}';
+    }
 }

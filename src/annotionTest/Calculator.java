069fa822da2b6452282b67241500310cc46c0f64
starlikerain <Zxcvbnm12123>
2020-04-12 11:11:18 +0800

    用注解实现异常捕获的基本原理2 补充

--- src/annotionTest/Calculator.java
+++ src/annotionTest/Calculator.java
@@ -4,7 +4,14 @@ public class Calculator {
 
     @Check
     public void add() {
+        String str = null;
+        str.toString();
         System.out.println("1 + 0 = " + (1 + 0));
+    }
+
+    @Check
+    public void sub() {
+        System.out.println("1 - 0 = " + (1 - 0));
     }
 
     @Check
